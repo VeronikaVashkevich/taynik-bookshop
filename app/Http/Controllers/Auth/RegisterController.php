120222a3891f7fa7a76d461aae2e220619cca3d3
VeronikaVashkevich <45391306+VeronikaVashkevich@users.noreply.github.com>
2022-02-12 18:01:49 +0300

    разграничение ролей админа и обычного пользователя

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,11 +64,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'is_admin' => false,
         ]);
+        $user->assignRole('user');
+        return $user;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/book-list', [IndexController::class, 'bookList']);
@@ -10,5 +12,11 @@ Route::get('/cart', [IndexController::class, 'cart']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/logout', [\App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard/dashboard');
+    });
+});
