097a8378164ab1fc2af4a6e2ff92046827c5e556
Veronika Vashkevich <veronik.vashkevich@gmail.com>
2022-04-20 13:26:31 +0300

    Ошибки авторизации на русском языке

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Пароль был восстановлен!',
+    'sent' => 'Вам отправлена ссылка для восстановления пароля!',
+    'throttled' => 'Пожалуйста, подождите.',
+    'token' => 'Неверный код восстановления.',
+    'user' => "Нет пользователя с такой электронной почтой.",
+
+];
